4ee3885d42b33c80eb18968504b8cec6b0a571b6
LDVSOFT <lapshin.dv@mail.ru>
2015-09-29 11:26:40 +0300

    Third squash of work Braces for everyone!
    Newlines for everybody
    
    Optimised generic Collections.map

--- src/main/java/ru/spbau/mit/Collections.java
+++ src/main/java/ru/spbau/mit/Collections.java
@@ -17,7 +17,7 @@ public abstract class Collections {
      * @param <R> type of element in result collection
      * @return result collection
      */
-    public static <T, R> Iterable<R> map(final Function1<? super T, ? extends R> f, final Iterable<T> src) {
+    public static <T, R> Iterable<R> map(final Function1<? super T, R> f, final Iterable<T> src) {
         List<R> result = new ArrayList<>();
         for (T element : src) {
             result.add(f.apply(element));
@@ -36,8 +36,9 @@ public abstract class Collections {
     public static <T> Iterable<T> filter(final Predicate<? super T> f, final Iterable<T> src) {
         List<T> result = new ArrayList<>();
         for (T element : src) {
-            if (f.apply(element))
+            if (f.apply(element)) {
                 result.add(element);
+            }
         }
         return result;
     }
@@ -94,8 +95,9 @@ public abstract class Collections {
      * Fold from right implementation.
      */
     private static <T, R> R foldr(final Function2<? super T, ? super R, ? extends R> f, R x, final Iterator<T> src) {
-        if (!src.hasNext())
+        if (!src.hasNext()) {
             return x;
+        }
         T element = src.next();
         return f.apply(element, foldr(f, x, src));
     }
