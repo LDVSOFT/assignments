a439eab688a387a2f848aa6af67c17c7a417fd45
Nikolay Obedin <dancingrobot84@gmail.com>
2015-09-30 14:30:08 +0300

    Add some notes and default ctor

--- src/main/java/ru/spbau/mit/TreeSetImpl.java
+++ src/main/java/ru/spbau/mit/TreeSetImpl.java
@@ -1,6 +1,9 @@
 package ru.spbau.mit;
 
 import java.util.AbstractSet;
+import java.util.Comparator;
 
 public class TreeSetImpl<E> extends AbstractSet<E> {
+    public TreeSetImpl(Comparator<E> comparator) {
+    }
 }
