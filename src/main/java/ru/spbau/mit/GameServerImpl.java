e9df514e9379d1534e76b436b899a6d491c78983
LDVSOFT <lapshin.dv@mail.ru>
2015-11-15 17:40:00 +0300

    Added GameServerImpl

--- src/main/java/ru/spbau/mit/GameServerImpl.java
+++ src/main/java/ru/spbau/mit/GameServerImpl.java
@@ -1,25 +1,83 @@
 package ru.spbau.mit;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.*;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 
 public class GameServerImpl implements GameServer {
+    protected Game game;
+    protected Map<String, Connection> clients = new Hashtable<>();
+    protected ReadWriteLock lock = new ReentrantReadWriteLock();
+
     public GameServerImpl(String gameClassName, Properties properties) {
-        throw new UnsupportedOperationException("TODO: implement");
+        try {
+            Class<?> gameClass = Class.forName(gameClassName);
+            game = (Game) gameClass.getConstructor(GameServer.class).newInstance(this);
+            for (String propName: properties.stringPropertyNames()) {
+                String value = properties.getProperty(propName);
+                if (value.matches("\\d+")) {
+                    // int setter
+                    gameClass.getMethod("set" + propName, Integer.TYPE).invoke(game, Integer.decode(value));
+                } else {
+                    // String setter
+                    gameClass.getMethod("set" + propName, String.class).invoke(game, value);
+                }
+            }
+        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException | ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException(String.format("Cannot create and setup %s instance.", gameClassName), e);
+        }
     }
 
     @Override
     public void accept(final Connection connection) {
-        throw new UnsupportedOperationException("TODO: implement");
+        final String id = Integer.toString(clients.size() + 1);
+        lock.writeLock().lock();
+        try {
+            clients.put(id, connection);
+        } finally {
+            lock.writeLock().unlock();
+        }
+        connection.send(id);
+        game.onPlayerConnected(id);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (! Thread.interrupted()) {
+                    try {
+                        String message = connection.receive(0);
+                        game.onPlayerSentMsg(id, message);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+        }).start();
     }
 
     @Override
     public void broadcast(String message) {
-        throw new UnsupportedOperationException("TODO: implement");
+        lock.readLock().lock();
+        try {
+            for (Connection connection : clients.values()) {
+                connection.send(message);
+            }
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     @Override
     public void sendTo(String id, String message) {
-        throw new UnsupportedOperationException("TODO: implement");
+        Connection connection;
+        lock.readLock().lock();
+        try {
+            connection = clients.get(id);
+        } finally {
+            lock.readLock().unlock();
+        }
+        connection.send(message);
     }
 }
