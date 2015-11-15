4e75022a15b8ac0ae55f02ac8920e6b764b6ec7e
LDVSOFT <lapshin.dv@mail.ru>
2015-11-15 17:38:56 +0300

    Fixed codestyle in test

--- src/test/java/ru/spbau/mit/TestHelloWorldServer.java
+++ src/test/java/ru/spbau/mit/TestHelloWorldServer.java
@@ -1,8 +1,8 @@
 package ru.spbau.mit;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import java.util.*;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class TestHelloWorldServer {
