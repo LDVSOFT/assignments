206c1d353aa9d04f55274a4660d8d5919553629e
LDVSOFT <lapshin.dv@mail.ru>
2015-11-16 16:27:56 +0300

    Rewrite GameServerImpl to send messages in thread of target connection
    It was not stated clearly which thread should send messages -- that one
    who sends messages or the initiator of message.
    I got the answer from the other group.

--- src/main/java/ru/spbau/mit/GameServerImpl.java
+++ src/main/java/ru/spbau/mit/GameServerImpl.java
@@ -7,9 +7,53 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 
 public class GameServerImpl implements GameServer {
+    public static final int TIMEOUT = 100;
     protected Game game;
-    protected Map<String, Connection> clients = new Hashtable<>();
+    protected Map<String, ConnectionHandler> clients = new Hashtable<>();
     protected ReadWriteLock lock = new ReentrantReadWriteLock();
+
+    private class ConnectionHandler implements Runnable {
+        private final Queue<String> toSend = new ArrayDeque<>();
+        private final String id;
+        private final Connection connection;
+
+        private ConnectionHandler(String id, Connection connection) {
+            this.id = id;
+            this.connection = connection;
+        }
+
+        private void send(String message) {
+            synchronized (toSend) {
+                toSend.add(message);
+            }
+        }
+
+        @Override
+        public void run() {
+            while (!Thread.interrupted()) {
+                synchronized (connection) {
+                    if (connection.isClosed())
+                        return;
+
+                    synchronized (toSend) {
+                        while (!toSend.isEmpty()) {
+                            connection.send(toSend.poll());
+                            if (connection.isClosed())
+                                return;
+                        }
+                    }
+                    try {
+                        String message = connection.receive(TIMEOUT);
+                        if (message != null) {
+                            game.onPlayerSentMsg(id, message);
+                        }
+                    } catch (InterruptedException e) {
+                        //Well, it sent nothing
+                    }
+                }
+            }
+        }
+    }
 
     public GameServerImpl(String gameClassName, Properties properties) {
         try {
@@ -38,37 +82,22 @@ public class GameServerImpl implements GameServer {
         final String id = Integer.toString(clients.size());
         lock.writeLock().lock();
         try {
-            clients.put(id, connection);
+            ConnectionHandler handler = new ConnectionHandler(id, connection);
+            clients.put(id, handler);
             connection.send(id);
             game.onPlayerConnected(id);
+            new Thread(handler).start();
         } finally {
             lock.writeLock().unlock();
         }
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (! Thread.interrupted()) {
-                    synchronized (connection) {
-                        if (connection.isClosed())
-                            return;
-                        try {
-                            String message = connection.receive(0);
-                            game.onPlayerSentMsg(id, message);
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
-                        }
-                    }
-                }
-            }
-        }).start();
     }
 
     @Override
     public void broadcast(String message) {
         lock.readLock().lock();
         try {
-            for (Connection connection : clients.values()) {
-                connection.send(message);
+            for (ConnectionHandler handler : clients.values()) {
+                handler.send(message);
             }
         } finally {
             lock.readLock().unlock();
@@ -77,13 +106,13 @@ public class GameServerImpl implements GameServer {
 
     @Override
     public void sendTo(String id, String message) {
-        Connection connection;
+        ConnectionHandler handler;
         lock.readLock().lock();
         try {
-            connection = clients.get(id);
+            handler = clients.get(id);
         } finally {
             lock.readLock().unlock();
         }
-        connection.send(message);
+        handler.send(message);
     }
 }
