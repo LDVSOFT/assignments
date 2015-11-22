3a22c8f76ac5f3899ab093adc6b817aa71faef2b
LDVSOFT <lapshin.dv@mail.ru>
2015-11-23 00:22:04 +0300

    Revert "Fixed codestyle in test"
    This reverts commit 4e75022a15b8ac0ae55f02ac8920e6b764b6ec7e.

--- src/test/java/ru/spbau/mit/TestHelloWorldServer.java
+++ src/test/java/ru/spbau/mit/TestHelloWorldServer.java
@@ -1,8 +1,8 @@
 package ru.spbau.mit;
 
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import java.util.*;
 
 
 public class TestHelloWorldServer {
