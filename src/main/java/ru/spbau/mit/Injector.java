853ece3f6f6b551bf1ba6dc20112c86ce0e3c171
Denis Zharkov <denis.zharkov@jetbrains.com>
2015-12-02 16:07:25 +0300

    Add test-02

---
+++ src/main/java/ru/spbau/mit/Injector.java
@@ -0,0 +1,15 @@
+package ru.spbau.mit;
+
+import java.util.List;
+
+
+public class Injector {
+
+    /**
+     * Create and initialize object of `rootClassName` class using classes from
+     * `implementationClassNames` for concrete dependencies.
+     */
+    public static Object initialize(String rootClassName, List<String> implementationClassNames) throws Exception {
+        throw new IllegalStateException();
+    }
+}
