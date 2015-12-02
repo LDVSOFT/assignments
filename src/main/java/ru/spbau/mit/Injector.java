4da72af3e270093f9528018f1e4ef47231836f6a
LDVSOFT <lapshin.dv@mail.ru>
2015-12-02 16:41:30 +0300

    Implemented the task

--- src/main/java/ru/spbau/mit/Injector.java
+++ src/main/java/ru/spbau/mit/Injector.java
@@ -1,15 +1,66 @@
 package ru.spbau.mit;
 
-import java.util.List;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Type;
+import java.util.*;
 
 
 public class Injector {
+
+    private static Object initializeInContext(
+            String interfaceName,
+            Map<String, Object> context,
+            List<String> implementationClassNames) throws Exception {
+        //Search for implementation
+        Class<?> targetClass = Class.forName(interfaceName);
+        Class<?> solutionClass = null;
+        for (String candidate : implementationClassNames) {
+            Class<?> candidateClass = Class.forName(candidate);
+            if (!targetClass.isAssignableFrom(candidateClass)) {
+                continue;
+            }
+            if (solutionClass != null) {
+                throw new AmbiguousImplementationException();
+            }
+            solutionClass = candidateClass;
+        }
+        if (solutionClass == null) {
+            throw new ImplementationNotFoundException();
+        }
+        String className = solutionClass.getName();
+
+        //Check if it was already created
+        if (context.containsKey(className)) {
+            Object instance = context.get(className);
+            if (instance == null) {
+                throw new InjectionCycleException();
+            }
+            return instance;
+        }
+
+        // To prevent cycles
+        context.put(className, null);
+
+        // Gather parameters recursively
+        Constructor<?> constructor = solutionClass.getConstructors()[0];
+        List<Object> params = new ArrayList<>();
+        for (Type type : constructor.getGenericParameterTypes()) {
+            params.add(initializeInContext(type.getTypeName(), context, implementationClassNames));
+        }
+
+        // Create instance
+        Object instance = constructor.newInstance(params.toArray());
+        context.put(className, instance);
+        return instance;
+    }
 
     /**
      * Create and initialize object of `rootClassName` class using classes from
      * `implementationClassNames` for concrete dependencies.
      */
     public static Object initialize(String rootClassName, List<String> implementationClassNames) throws Exception {
-        throw new IllegalStateException();
+        List<String> classNames = new ArrayList<>(implementationClassNames);
+        classNames.add(rootClassName);
+        return initializeInContext(rootClassName, new HashMap<String, Object>(), classNames);
     }
 }
